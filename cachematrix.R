78bd6f04dc9a468fbd982d8b411bcae6e51fb9dd
Courtney Rowlands <54596326+crowlands1010@users.noreply.github.com>
2019-08-30 07:55:35 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,35 @@
 ## This assignment is to write a pair of functions that will cache
 ## the inverse of a matrix. The purpose of this assignment is to
 ## fulfill the requirements set forth in the second assignment in 
-## the R Programming course on Coursera.
+## the R Programming course on Coursera -- Data Specialization --
+## Johns Hopkins University
 
-## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## Instructions from README.md (for convenience)
+## 1.  Fork the GitHub repository containing the stub R files at
+## [https://github.com/rdpeng/ProgrammingAssignment2](https://github.com/rdpeng/ProgrammingAssignment2)
+## to create a copy under your own account. 
+## 2.  Clone your forked GitHub repository to your computer so that you can
+## edit the files locally on your own machine.
+## 3.  Edit the R file contained in the git repository and place your
+## solution in that file (please do not rename the file).
+## 4.  Commit your completed R file into YOUR git repository and push your
+## git branch to the GitHub repository under your account.
+## 5.  Submit to Coursera the URL to your GitHub repository that contains
+## the completed R code for the assignment.
+
+## makeCacheMatrix: This function creates a special "matrix" object
+## that can cache its inverse..
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
 }
+      
 
-
-## cacheSolve: This function is to compute the inverse of the special matrix
-## returned by the above function, makeCacheMatrix. If the inverse has already
-## been calculated, then cacheSolve should retrieve inverse from cache.
+## This function computes the inverse of the special
+## "matrix" returned by `makeCacheMatrix` above. If the inverse has
+## already been calculated (and the matrix has not changed), then
+## `cacheSolve` should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
 }
